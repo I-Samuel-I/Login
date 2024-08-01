a6122da89536ae4a7a358fc95d09401181892bba
Samuel Gomes <SamPongPie@gmail.com>
2024-08-01 17:21:28 -0300

    Eye password implementado e polimento do radio/label

--- src/assets/components/Forms/Extra/Extra.jsx
+++ src/assets/components/Forms/Extra/Extra.jsx
@@ -1,10 +1,35 @@
 import styles from './Extra.module.css'
+import { useState } from 'react'
 export default function Extra(){
+    //Váriavel para o state do radio.
+    const [RememberSelect, setRememberSelect] = useState(false);
+    
+    //Ambos fazem o mesmo objetivo, mas não recomendado o abaixo.
+    // const RadioClick = () =>{
+    //     setRememberSelect (!RememberSelect);
+
+    // }
+    
+    
+    //Guarda o resultado anterior, e depois reverte
+    // True para False | False para True
+    // Retorna o valor atual para o radio/label.
+    const RadioClick = () =>{
+        setRememberSelect(prevState => !prevState);
+        // Função de atualização, forma indireta.
+    }
+
     return(
     <form className={styles.Container}> 
         <div className={styles.Content}>
-            <input className={styles.PositionRadio} type="radio" name="remember" id="remember" ></input>
-            <label className={styles.PositionRadio} htmlfor="remember" >Lembre de mim</label>
+            <input className={styles.PositionRadio} 
+            type="radio" name="remember" id="remember" 
+            checked={RememberSelect}
+            onClick={RadioClick}></input>
+            
+            <label className={styles.PositionRadio} htmlfor="remember" onClick={RadioClick}>
+                Lembre de mim
+            </label>
         </div>
         <ul className={styles.Content}>
             <a className={styles.ContainerLink} href="#" target="_blank">Esqueceu sua senha?</a>
--- src/assets/components/Forms/Input/Login.jsx
+++ src/assets/components/Forms/Input/Login.jsx
@@ -1,17 +1,43 @@
 import styles from './InputStyles/Login.module.css'
+import { useState } from 'react';
 export default function Login(){
+    const [ShowPassword,setShowPassword] = useState(false);
+
+    function Eye() {
+    const link = document.createElement("link");
+    link.id ="bootstrap-icons"
+    link.rel = "stylesheet"
+    link.href = "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css";            
+    document.head.appendChild(link);            
+}   
+    Eye();
+    const PasswordVisibility = () =>{
+        setShowPassword(prevState => !prevState)
+    }
+    
     return(
-        <form>
+        
+        
+        <form>  
+              
             <div className={styles.Container}>
                 <p>E-mail</p>
                 <input className={styles.GlobalInput} type="email"placeholder="exemplo@gmail.com" 
-                name="email" minLength="20"></input>
+                name="email" minLength="20" autoComplete='on' required/>
             </div> 
             <div className={styles.Container}>
                 <p>Senha</p>
-                <input className={styles.GlobalInput} type="password" placeholder="0123456789" 
-                name="password" minLength="8" 
-                maxLength="70"></input>
+                <input 
+                className={styles.GlobalInput} 
+                type={ShowPassword ? "text" : "password"} 
+                placeholder="0123456789" 
+                name="password" minLength="8" maxLength="70" required/>
+                <i 
+                className={` ${ShowPassword ? "bi-eye-slash" : "bi-eye"} 
+                ${styles.EyeIcon}`}
+                onClick={PasswordVisibility}>
+                </i>
+                
             </div>
         </form>
     )
